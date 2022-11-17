52749b68275155a8cb8c2df898c858c24fde2be9
ywyang236 <118434629+ywyang236@users.noreply.github.com>
2022-11-17 21:03:30 +0800

    Update entw_Cambridge.js

--- entw_Cambridge.js
+++ entw_Cambridge.js
@@ -8,7 +8,8 @@ class encn_Cambridge_tc {
 
     async displayName() {
         let locale = await api.locale();
-        if (locale.indexOf('TW') != -1) return '劍橋辭典(繁體中文)';
+        if (locale.indexOf('CN') != -1) return '剑桥英汉双解(繁体)';
+        if (locale.indexOf('TW') != -1) return '劍橋英漢雙解(繁体)';
         return 'Cambridge EN->CN Dictionary (TC)';
     }
 
@@ -35,7 +36,7 @@ class encn_Cambridge_tc {
                 return node.innerText.trim();
         }
 
-        let base = 'https://dictionary.cambridge.org/zht/';
+        let base = 'https://dictionary.cambridge.org/search/english-chinese-traditional/direct/?q=';
         let url = base + encodeURIComponent(word);
         let doc = '';
         try {
@@ -55,13 +56,17 @@ class encn_Cambridge_tc {
             let reading = '';
             let readings = entry.querySelectorAll('.pron .ipa');
             if (readings) {
+                let reading_uk = T(readings[0]);
                 let reading_us = T(readings[1]);
-                reading = (reading_us) ? `${reading_us} ` : '';
+                reading = (reading_uk || reading_us) ? `UK[${reading_uk}] US[${reading_us}] ` : '';
             }
             let pos = T(entry.querySelector('.posgram'));
             pos = pos ? `<span class='pos'>${pos}</span>` : '';
+            audios[0] = entry.querySelector(".uk.dpron-i source");
+            audios[0] = audios[0] ? 'https://dictionary.cambridge.org' + audios[0].getAttribute('src') : '';
+            //audios[0] = audios[0].replace('https', 'http');
             audios[1] = entry.querySelector(".us.dpron-i source");
-            audios[1] = audios[1] ? 'https://dictionary.cambridge.org/zht/' + audios[1].getAttribute('src') : '';
+            audios[1] = audios[1] ? 'https://dictionary.cambridge.org' + audios[1].getAttribute('src') : '';
             //audios[1] = audios[1].replace('https', 'http');
 
             let sensbodys = entry.querySelectorAll('.sense-body') || [];
@@ -96,13 +101,12 @@ class encn_Cambridge_tc {
                         if (examps.length > 0 && this.maxexample > 0) {
                             definition += '<ul class="sents">';
                             for (const [index, examp] of examps.entries()) {
-                                if (index > this.maxexample - 0) break; // to control only 2 example sentence.
+                                if (index > this.maxexample - 1) break; // to control only 2 example sentence.
                                 let eng_examp = T(examp.querySelector('.eg'));
                                 let chn_examp = T(examp.querySelector('.trans'));
                                 definition += `<li class='sent'><span class='eng_sent'>${eng_examp.replace(RegExp(expression, 'gi'),`<b>${expression}</b>`)}</span><span class='chn_sent'>${chn_examp}</span></li>`;
                             }
                             definition += '</ul>';
-                           
                         }
                         definition && definitions.push(definition);
                     }
@@ -133,7 +137,6 @@ class encn_Cambridge_tc {
             let youdao = getYoudao(doc); //Combine Youdao Concise English-Chinese Dictionary to the end.
             let ydtrans = getYDTrans(doc); //Combine Youdao Translation (if any) to the end.
             return [].concat(youdao, ydtrans);
-            font-variant-east-asian: traditional;
         } catch (err) {
             return [];
         }
@@ -221,16 +224,16 @@ class encn_Cambridge_tc {
     renderCSS() {
         let css = `
             <style>
-                div.phrasehead{margin: 2px 0;}
+                div.phrasehead{margin: 2px 0;font-weight: bold;}
                 span.star {color: #FFBB00;}
-                span.pos  {text-transform:lowercase; font-size:0.9em; margin-right:5px; padding:2px 4px;}
+                span.pos  {text-transform:lowercase; font-size:0.9em; margin-right:5px; padding:2px 4px; color:white; background-color:#0d47a1; border-radius:3px;}
                 span.tran {margin:0; padding:0;}
                 span.eng_tran {margin-right:3px; padding:0;}
-                span.chn_tran {color:#black;}
-                ul.sents {font-size:0.8em; list-style:square inside; margin:3px 0;padding:5px;}
+                span.chn_tran {color:#0d47a1;}
+                ul.sents {font-size:0.8em; list-style:square inside; margin:3px 0;padding:5px;background:rgba(13,71,161,0.1); border-radius:5px;}
                 li.sent  {margin:0; padding:0;}
                 span.eng_sent {margin-right:5px;}
-                span.chn_sent {color:#black;}
+                span.chn_sent {color:#0d47a1;}
             </style>`;
         return css;
     }
