a98a415c39b5dc1decb569b996cacea8bcf7caef
ywyang236 <118434629+ywyang236@users.noreply.github.com>
2022-11-17 20:44:41 +0800

    Update entw_Cambridge.js

--- entw_Cambridge.js
+++ entw_Cambridge.js
@@ -8,7 +8,6 @@ class encn_Cambridge_tc {
 
     async displayName() {
         let locale = await api.locale();
-        if (locale.indexOf('CN') != -1) return '剑桥英汉双解(繁体)';
         if (locale.indexOf('TW') != -1) return '劍橋辭典(繁體中文)';
         return 'Cambridge EN->CN Dictionary (TC)';
     }
@@ -56,14 +55,13 @@ class encn_Cambridge_tc {
             let reading = '';
             let readings = entry.querySelectorAll('.pron .ipa');
             if (readings) {
-                let reading_uk = T(readings[0]);
                 let reading_us = T(readings[1]);
                 reading = (reading_us) ? `${reading_us} ` : '';
             }
             let pos = T(entry.querySelector('.posgram'));
             pos = pos ? `<span class='pos'>${pos}</span>` : '';
             audios[1] = entry.querySelector(".us.dpron-i source");
-            audios[1] = audios[1] ? 'https://dictionary.cambridge.org' + audios[1].getAttribute('src') : '';
+            audios[1] = audios[1] ? 'https://dictionary.cambridge.org/zht/' + audios[1].getAttribute('src') : '';
             //audios[1] = audios[1].replace('https', 'http');
 
             let sensbodys = entry.querySelectorAll('.sense-body') || [];
@@ -94,7 +92,7 @@ class encn_Cambridge_tc {
                         if (examps.length > 0 && this.maxexample > 0) {
                             definition += '<ul class="sents">';
                             for (const [index, examp] of examps.entries()) {
-                                if (index > this.maxexample - 1) break; // to control only 2 example sentence.
+                                if (index > this.maxexample - 0) break; // to control only 2 example sentence.
                                 let eng_examp = T(examp.querySelector('.eg'));
                                 let chn_examp = T(examp.querySelector('.trans'));
                                 definition += `<li class='sent'><span class='eng_sent'>${eng_examp.replace(RegExp(expression, 'gi'),`<b>${expression}</b>`)}</span><span class='chn_sent'>${chn_examp}</span></li>`;
